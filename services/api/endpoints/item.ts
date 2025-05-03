8d363429eb2f8a197b794596e23a50be71508d8c
Kirill Matusevich <kirilenkich@gmail.com>
2025-05-03 18:42:25 +0300

    creating driver from web

--- services/api/endpoints/item.ts
+++ services/api/endpoints/item.ts
@@ -4,6 +4,17 @@ type productProps = {
     id: string
 }
 
+type newDriverProps = {
+  firstName: string
+  secondName: string
+  sex: string
+  experience: number
+  age: number
+  category: string[],
+  login: string,
+  password: string,
+}
+
 export const itemService = {
   async getProduct() {
     return apiClient.get('/getProducts');
@@ -13,5 +24,8 @@ export const itemService = {
   },
   async getAllDrivers() {
     return apiClient.get('/getAllDrivers');
+  },
+  async addDriver(newDriver: newDriverProps) {
+    return apiClient.post("/addDriver", newDriver)
   }
 };
--- src/pages/driver/add.tsx
+++ src/pages/driver/add.tsx
@@ -1,15 +1,109 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, TextField, ToggleButton, ToggleButtonGroup, Typography } from "@mui/material";
+import Header from "../../components/Header";
+import { useState } from "react";
+import { itemService } from "../../../services/api/endpoints/item";
+import { slugify } from 'transliteration';
 
 export default function AddDriver() {
-    return (
-        <>
-        <Box sx={{ 
-            border: "2px solid lightgrey",
-            borderRadius: "24px",
-            padding: 2,
+  const [firstName, setFirstName] = useState<string>("")
+  const [secondName, setSecondName] = useState<string>("")
+  const [sex, setSex] = useState<string>()
+  const [experience, setExperience] = useState<number>()
+  const [age, setAge] = useState<number>()
+  const [category, setCategory] = useState<string[]>([])
+
+  const handleSexChange = (
+    _event: React.MouseEvent<HTMLElement>,
+    newSex: string,
+  ) => {
+    if (newSex !== null) {
+      setSex(newSex);
+    }
+  };
+
+  function generateSecurePassword(length: number = 10): string {
+    const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    const result = [];
+    const values = new Uint32Array(length);
+    window.crypto.getRandomValues(values);
+  
+    for (let i = 0; i < length; i++) {
+      const index = values[i] % charset.length;
+      result.push(charset.charAt(index));
+    }
+  
+    return result.join('');
+  }
+
+  const handleAddDriver = () => {
+    console.log("info: " + firstName + secondName, sex, experience, age, category)
+    if(sex == undefined) return;
+    if(experience == undefined) return;
+    if(age == undefined) return;
+
+    const login = slugify(`${firstName}-${secondName}`).toLowerCase();
+    const password = generateSecurePassword();
+
+    itemService.addDriver({
+      firstName: firstName,
+      secondName: secondName,
+      sex: sex,
+      experience: experience,
+      age: age,
+      category: category,
+      login: login,
+      password: password,
+    })
+    .then(res => console.log(res))
+    .catch()
+    .finally()
+  }
+
+  return (
+    <>
+      <Box sx={{
+        padding: 2,
+        display: "flex",
+        flexDirection: "column",
+      }}>
+        <Header title="Добавить нового водителя в систему"></Header>
+        <Box sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
         }}>
-            <Typography>Добавить нового водителя в систему</Typography>
-            </Box>
-        </>
-    )
+          <TextField label="Имя" variant="outlined"  value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
+          <TextField label="Фамилия" variant="outlined" value={secondName} onChange={(e) => setSecondName(e.target.value)}/>
+          <ToggleButtonGroup
+            value={sex}
+            exclusive
+            onChange={handleSexChange}
+          >
+            <ToggleButton value="male">М</ToggleButton>
+            <ToggleButton value="female">Ж</ToggleButton>
+          </ToggleButtonGroup>
+          <TextField label="Опыт работы" variant="outlined" value={experience} onChange={(e) => setExperience(+e.target.value)}/>
+          <TextField label="Возраст" variant="outlined" value={age} onChange={(e) => setAge(+e.target.value)}/>
+          <TextField label="Водительские категории(через запятую)" variant="outlined" value={category.join(",")} onChange={(e) => setCategory((e.target.value).split(","))}/>
+        </Box>
+
+        <Button
+          onClick={() => handleAddDriver()}
+          sx={{
+            backgroundColor: "#8EBB8E",
+            marginTop: 2,
+            color: "#000",
+            borderRadius: "12px",
+            py: 1,
+            px: 3,
+            textDecoration: "none",
+            "&:hover": {
+              backgroundColor: "#1C771C",
+            }
+          }}>
+          Добавить водителя
+        </Button>
+      </Box>
+    </>
+  )
 }
