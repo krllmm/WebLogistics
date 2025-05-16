9f24d41ab51f35bd3f5f59ed8fe8b256872dc39b
Kirill Matusevich <kirilenkich@gmail.com>
2025-05-16 13:50:36 +0300

    disabling getting data if not authenticated

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,8 +1,25 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar"
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+
+interface Agent {
+  login: string,
+  super_rights: boolean,
+}
 
 export default function Layout() {
+  const [user, setUser] = useState<Agent>({ "login": "none", "super_rights": false })
+
+  useEffect(() => {
+    checkAuth()
+  }, [])
+
+  const checkAuth = async () => {
+    const user = await JSON.parse(localStorage.getItem("user") || `{"login": "none", "super_rights": "false"}`)
+    setUser({ "login": user["login"], "super_rights": user["super_rights"] })
+  }
+
   return (
     <>
       <Box sx={{ display: 'flex', minHeight: '100vh' }}>
@@ -12,7 +29,22 @@ export default function Layout() {
         </Box>
 
         <Box sx={{ flexGrow: 1, padding: "12px" }}>
-          <Outlet />
+          {
+            user.login === "none" ?
+              <Box sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "100%",
+              }}>
+                <Box sx={{ flex: 1, textAlign: "center" }}>
+                  <Typography sx={{ fontSize: 20, }}>
+                    Для просмотра данных необходимо войти в систему
+                  </Typography>
+                </Box>
+              </Box>
+              : <Outlet />
+          }
         </Box>
 
       </Box>
