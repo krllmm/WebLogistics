c981ad70fda558795abed317abb46375d826c9b8
Kirill Matusevich <kirilenkich@gmail.com>
2025-06-05 14:50:27 +0300

    set link to server

--- services/api/config.ts
+++ services/api/config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  BASE_URL: "http://127.0.0.1:5000",
+  BASE_URL: "https://logisticsserver.onrender.com",
   // BASE_URL: "https://python-patient-solely.ngrok-free.app",
   TIMEOUT: 5000,
   HEADERS: {
